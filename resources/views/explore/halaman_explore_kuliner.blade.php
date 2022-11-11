89ec49e34263953bff10a01c421238917ff56b57
ayusetyaningsih <ayusetyaningsih@student.uns.ac.id>
2022-11-11 18:18:49 +0700

    tambah wisata

--- resources/views/explore/halaman_explore_kuliner.blade.php
+++ resources/views/explore/halaman_explore_kuliner.blade.php
@@ -27,7 +27,7 @@
             {!! Toastr::message() !!}
 
             <!--==================== DISCOVER ====================-->
-            <section class="discover section" id="explore_kuliner">
+            <section class="discover section" id="discover">
                 <h2 class="section__title" id="#explore_kuliner">Temukan Tempat Kuliner <br> Di Restoran Paling Menarik</h2>
 
                 <div class="card-group">
@@ -39,7 +39,7 @@
                                     Gambar Tidak Tersedia
                                 @else
                                     <img src="{{ asset('images') }}/{{ $tempat->image }}" alt="Responsive image"
-                                        class="img-thumbnail">
+                                        class="discover__img">
                                 @endif
                                 <div class="discover__data">
                                     <h2 class="discover__title">{{ $tempat->name }}</h2>
